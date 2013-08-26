ea94e663ce090d932ac344d98f5b47de16b3814f
Eduardo Reche <edu.reche@gmail.com>
2013-08-25 21:59:42 -0300

    - ran brakeman and removed secret_token from   the source
    ** listening Maxwell's Silver Hammer - The Beatles

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Fut::Application.config.secret_token = '27d24050b514b6ff3e64eeda2bccec4de195e7c0a64c435e6635219e2f3748dc2ecf2a3847c35f1f7518e4c405468c835328c72e760215292db8bf2629b93b21'
+Fut::Application.config.secret_token = ENV['SESSION_SECRET']
